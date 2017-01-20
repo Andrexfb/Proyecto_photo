d6d8ff0b014530ef7de4fd9938938d8f2132cf78
Andrexfb <andresfuba3@gmail.com>
2017-01-20 14:04:43 -0600

    Subir foto 3

--- app/controllers/photo_controller.php
+++ app/controllers/photo_controller.php
@@ -8,8 +8,8 @@
 				mkdir("../../resources/fotos/".$categoria."/".$nombre."/", 0700, true);
 				$foto=$_FILES["foto"]["name"];
 				$ruta=$_FILES["foto"]["tmp_name"];
-				$destino="../../resources/fotos/".$categoria."/".$nombre."/".$foto;
-				copy($ruta,$destino);
+				$destino="resources/fotos/".$categoria."/".$nombre."/".$foto;
+				copy($ruta,"../../".$destino);
 				$new_avion_data= array(
 						'nombre'=>$nombre,
 						'destino'=>$destino
